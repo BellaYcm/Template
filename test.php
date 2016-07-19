650a66c176277a0c53bb354c8183d8d32b3a1562
sunke <sunkejl@live.com>
2016-07-19 13:47:26 +0800

    refs template

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: sunke
+     * Date: 2016/7/19
+     * Time: 10:52
+     */
+    include "Template.php";
+    include "CompileTools.php";
+    $tpl = new Template(new CompileTools());
+    $tpl->assign("name","sk");
+    $tpl->show('index');
